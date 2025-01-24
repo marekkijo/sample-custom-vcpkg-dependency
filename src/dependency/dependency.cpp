ebb70166caa49e6b14444c3c2816f574631334cc
Marek Kijo <marekkijo@gmail.com>
2025-01-24 18:27:23 +0100

    1: Added dummy libdependency lib

---
+++ src/dependency/dependency.cpp
@@ -0,0 +1,7 @@
+#include "dependency/dependency.hpp"
+
+#include <iostream>
+
+namespace dependency {
+void do_something() { std::cout << "Doing something" << std::endl; }
+} // namespace dependency
---
+++ src/dependency/dependency.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace dependency {
+void do_something();
+}
